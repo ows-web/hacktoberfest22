e2ca0a323b6a377b11f2a4782e24af40f72fe63c
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2021-10-20 09:58:29 -0300

    chore: create multiple array example

---
+++ php/arrays/example-02.php
@@ -0,0 +1,17 @@
+<?php 
+
+	$carros[0][0] = "GM";
+	$carros[0][1] = "Onix";
+	$carros[0][2] = "Camaro";
+	$carros[0][3] = "Cobalt";
+
+	$carros[1][0] = "Ford";
+	$carros[1][1] = "Fiesta";
+	$carros[1][2] = "Fusion";
+	$carros[1][3] = "Eco Sport";
+
+	echo $carros[0][3];
+
+	echo end($carros[1]);
+
+ ?>

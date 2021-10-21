ddfbae9bbe4461fe0a1eaaef13bd83706b527fd9
Fazil Khan <76592655+comedianfazil@users.noreply.github.com>
2021-10-21 11:19:56 +0530

    Added Identify Even-Odd numbers

---
+++ php/evenodd.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Even Odd</title>
+</head>
+<body>
+<?php   
+$a=15;
+if ($a%2==0)
+ {
+	echo"$a is Even number";
+} else
+ {
+
+	echo"$a is Odd number";
+}
+
+?>
+</body>
+</html>

453828b2510d136ab651fdc549e4aa10bd9df5f3
Fazil Khan <76592655+comedianfazil@users.noreply.github.com>
2021-10-21 12:39:25 +0530

    Create simpleform.php

---
+++ php/simpleform.php
@@ -0,0 +1,17 @@
+
+<html>
+<body>
+<form method="get" action="success.php">
+	Name :<input type="text" name="name"><br><br>
+	<label>Select your Hobbies:</label><br>
+	<input type="checkbox" name="cricket" value="Cricket" checked="true">Cricket<br>
+	<input type="checkbox" name="football" value="Football">FootBall<br>
+	<input type="checkbox" name="basketball" value="Cricket">Basket Ball<br><br>
+
+	<label>Select Your Gender:</label><br>
+	<input type="radio" name="gender" value="male" checked>Male<br>
+	<input type="radio" name="gender" value="female">Female<br><br>
+	<input type="submit" value="submit">
+</form>
+</body>
+</html>
